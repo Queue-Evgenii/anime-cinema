fe9e88fadb333e794728b463c0943d6c59417f91
Queue-Evgenii <genadolgii228@gmail.com>
2025-01-17 23:03:27 +0200

    anime view implemented

--- src/views/anime/anime.module.js
+++ src/views/anime/anime.module.js
@@ -4,17 +4,23 @@ angular.module('animeView', [
   'api'
 ])
   .controller('animeController', [
-    '$scope', '$routeParams', 'animeGetApi',
-    function($scope, $routeParams, animeGetApi) {
+    '$scope', '$routeParams', '$sce', 'animeGetApi',
+    function($scope, $routeParams, $sce, animeGetApi) {
       $scope.animeItem = {};
       animeGetApi.getAnimeById($routeParams.id)
         .then(data => {
           $scope.$apply(function() {
             $scope.animeItem = data.data;
+            $scope.animeItem.trailer.embed_url = $sce.trustAsResourceUrl($scope.animeItem.trailer.embed_url);
           })
         })
         .catch(err =>
           console.log('getAnimeById Err', err)
         );
+
+      $scope.isPlaying = false;
+      $scope.trailerHeight = 600;
+
+      $scope.playVideo = () => $scope.isPlaying = true;
     }
   ]);
